b2d74e0c31af2cd5574ba748be32e2f42fdd10f9
Keisyatm <keisyatmawa@gmail.com>
2025-12-26 16:59:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include 'koneksi.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
+</head>
+<body>
+    <?php include "navbar.php"; ?>
+    <div class="container mt-5">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active" aria-current="page">
+                    <i class="fas fa-home"></i> Home
+                </li>
+            </ol>
+        </nav>
+        <h2>Halo <?php  echo $_SESSION['username']; ?> </h2>
+    </div>
+
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,10 @@
 <?php
-$con = mysqli_connect(hostname: "localhost", username: "root", password: "", database: "abqary");
+$con = mysqli_connect("localhost", "root", "", "abqary");
 
 // Check connection
 if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " .mysqli_connect_eror();
-  exit();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
 }
 ?>
+
